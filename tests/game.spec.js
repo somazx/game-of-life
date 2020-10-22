a3e0f979e4d00a9e658c6b221f89b7cf3d4d8aad
Andy Koch <koch.andy@gmail.com>
2020-10-22 14:40:31 -0700

    refactor tests to use describe/test/it

--- tests/game.spec.js
+++ tests/game.spec.js
@@ -3,31 +3,34 @@ import { Grid } from "src/core/grid.js"
 
 let game = new Game()
 
-it('Game to be a thing with a grid', () => {
-  expect(game).toBeInstanceOf(Game)
+describe('Game', () => {
+  it('initializes with a Grid', () => {
+    expect(game).toBeInstanceOf(Game)
 
-  expect(game.grid).toBeInstanceOf(Grid)
-});
+    expect(game.grid).toBeInstanceOf(Grid)
+  });
 
-it('randCoords', () => {
-  let coords = game.randCoords()
-  expect(coords).toBeInstanceOf(Array)
-  expect(coords[0]).toEqual(
-    expect.objectContaining({
-      x: expect.any(Number),
-      y: expect.any(Number),
-    })
-  )
-});
+  test('randCoords()', () => {
+    let coords = game.randCoords()
+    expect(coords).toBeInstanceOf(Array)
+    expect(coords[0]).toEqual(
+      expect.objectContaining({
+        x: expect.any(Number),
+        y: expect.any(Number),
+      })
+    )
+  });
 
 
-it('populateLife', () => {
-  let coords = game.randCoords()
-  game.populateLife(coords)
+  test('populateLife()', () => {
+    let coords = game.randCoords()
+    game.populateLife(coords)
 
-  let cell = game.grid.getCell(coords[1])
-  cell.alive = true
+    let cell = game.grid.getCell(coords[1])
+    cell.alive = true
 
-  cell = game.grid.getCell(coords[1])
-  expect(cell.alive).toBe(true)
+    cell = game.grid.getCell(coords[1])
+    expect(cell.alive).toBe(true)
+  })
 })
+
--- tests/grid.spec.js
+++ tests/grid.spec.js
@@ -1,29 +1,36 @@
 import {Grid, Row, Cell} from "src/core/grid.js"
 
-it('Grid to initialize with Rows and Cells', () => {
-  let grid = new Grid(10, 10);
+describe('Grid', () => {
+  it('initializes with Rows and Cells', () => {
+    let grid = new Grid(10, 10);
 
-  expect(grid.rows.length).toBe(10)
-  expect(grid.rows[9]).toBeInstanceOf(Row)
+    expect(grid.rows.length).toBe(10)
+    expect(grid.rows[9]).toBeInstanceOf(Row)
 
-  expect(grid.rows[9].cols.length).toBe(10)
-  expect(grid.rows[9].cols[9]).toBeInstanceOf(Cell)
+    expect(grid.rows[9].cols.length).toBe(10)
+    expect(grid.rows[9].cols[9]).toBeInstanceOf(Cell)
 
-  expect(grid.getCell({x: 0, y: 0})).toBeInstanceOf(Cell)
-  expect(grid.getCell({x: 0, y: 0}).x).toBe(0)
-  expect(grid.getCell({x: 0, y: 3}).y).toBe(3)
-});
-
-it("Row has index property", () => {
-  let grid = new Grid(10, 10);
-
-  expect(grid.rows[1].rowIndex).toBe(1)
+    expect(grid.getCell({x: 0, y: 0})).toBeInstanceOf(Cell)
+    expect(grid.getCell({x: 0, y: 0}).x).toBe(0)
+    expect(grid.getCell({x: 0, y: 3}).y).toBe(3)
+  });
 })
 
-it("Cell can return x and y", () => {
-  let grid = new Grid(10, 10);
+describe('Row', () => {
+  it("has rowIndex property", () => {
+    let grid = new Grid(10, 10);
 
-  expect(grid.rows[9].cols[1].x).toBe(1)
-  expect(grid.rows[9].cols[1].y).toBe(9)
+    expect(grid.rows[1].rowIndex).toBe(1)
+  })
+})
+
+
+describe('Cell', () => {
+  it("x and y getters", () => {
+    let grid = new Grid(10, 10);
+
+    expect(grid.rows[9].cols[1].x).toBe(1)
+    expect(grid.rows[9].cols[1].y).toBe(9)
+  })
 })
 
